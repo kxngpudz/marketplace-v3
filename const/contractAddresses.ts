94fb792185d059c01bce778439026e3641720bfa
kxngpudz <71802085+kxngpudz@users.noreply.github.com>
2023-06-11 23:32:03 -0500

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -7,12 +7,14 @@ export const NETWORK = Mumbai;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x6604bd9D7770035f26B4ACeab2C746fdCE166473";
+export const MARKETPLACE_ADDRESS = "0xE77968C6dAc576E8bbeb9f4dcF5DE7a877acc0a9";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0xFfd9bAddF3f6e427EfAa1A4AEC99131078C1d683";
+  "0x601e7750a9E412aEBa4E832Ba715565c1e1D0a4C";
+
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
 export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+
