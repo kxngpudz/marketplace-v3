6735095cf254078244cdb58db093746eda243c77
kxngpudz <71802085+kxngpudz@users.noreply.github.com>
2023-06-19 11:03:53 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,6 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-/**
- * Landing page with a simple gradient background and a hero asset.
- * Free to customize as you see fit.
- */
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
